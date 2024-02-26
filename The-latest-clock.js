b60b691d624e890561902c789ce2f2e5535eef81
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-25 21:02:11 -0800

    Update The-latest-clock.js

--- The-latest-clock.js
+++ The-latest-clock.js
@@ -11,3 +11,21 @@ Notes:
   Result should be a valid 24-hour time, between 00:00 and 23:59.
   Only inputs which have valid answers are tested.
 */
+
+
+// Solution
+
+const latestClock = (a, b, c, d) => {
+  const validTimeReg = /(?:[01]\d|2[0-3]):[0-5]\d/;
+  
+  const allCombos = [];
+  const makeCombos = (dgts, str = '') => {
+    !dgts.length && validTimeReg.test(str) && allCombos.push(str);
+    dgts.length === 2 && (str += ':');
+    dgts.forEach((dgt, idx) => makeCombos(dgts.filter((_, i) => i !== idx), str + dgt));
+  }
+  
+  makeCombos([a, b, c, d]);
+  
+  return allCombos.sort().at(-1) || '';
+}
